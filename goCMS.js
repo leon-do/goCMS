554b44ea11c373d25e4c6414e05359841bdefc4e
Leon Do <leondo123@gmail.com>
2020-03-21 19:24:18 -0400

    Update goCMS.js

--- goCMS.js
+++ goCMS.js
@@ -1,17 +1,17 @@
-  function goCMS(id, url) {
-    let request = new XMLHttpRequest();
-    request.open("GET", url);
-    request.send();
-    request.onload = () => {
-      if (request.status === 200) {
-        const html = request.response;
-        // throw away header and footer
-        const start = html.indexOf('<div id="contents">');
-        const end = html.indexOf('<div id="footer">');
-        // display
-        document.getElementById(id).innerHTML = html.slice(start, end);
-      } else {
-        reject(`error ${request.status} ${requets.statusText})`);
-      }
-    };
-  }
+function goCMS(id, url) {
+  let request = new XMLHttpRequest();
+  request.open("GET", url);
+  request.send();
+  request.onload = () => {
+    if (request.status === 200) {
+      const html = request.response;
+      // throw away header and footer
+      const start = html.indexOf('<div id="contents">');
+      const end = html.indexOf('<div id="footer">');
+      // display
+      document.getElementById(id).innerHTML = html.slice(start, end);
+    } else {
+      reject(`error ${request.status} ${requets.statusText})`);
+    }
+  };
+}
